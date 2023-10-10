e098903d9af74500dff5e9d88a2cc595b9d2f3b7
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 15:50:58 +0800

    Create RecursiveBinarySearch.cpp

---
+++ RecursiveBinarySearch.cpp
@@ -0,0 +1,7 @@
+#include "RecursiveBinarySearch.h"
+
+bool RecursiveBinarySearch::search(std::vector<int> list, int target) {
+    // Implement the recursive binary search algorithm here
+    // ...
+    return false;
+}
