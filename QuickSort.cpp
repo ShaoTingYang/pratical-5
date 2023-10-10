6e82a2c9b82d71d7e3720023b1a09957cae3fd14
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 15:59:42 +0800

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,7 +1,36 @@
-#include "QuickSort.h"
+#include <vector>
 
-std::vector<int> QuickSort::sort(std::vector<int> list) {
-    // Implement the Quick Sort algorithm here
-    // ...
-    return list;
-}
+class QuickSort {
+public:
+    static std::vector<int> sort(std::vector<int> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+        
+        int pivot = list[list.size() / 3]; // Choose the third value as the pivot
+        std::vector<int> less, equal, greater;
+        
+        for (int num : list) {
+            if (num < pivot) {
+                less.push_back(num);
+            } else if (num == pivot) {
+                equal.push_back(num);
+            } else {
+                greater.push_back(num);
+            }
+        }
+        
+        std::vector<int> sorted;
+        sorted = concatenate(sort(less), equal);
+        sorted = concatenate(sorted, sort(greater));
+        
+        return sorted;
+    }
+
+private:
+    static std::vector<int> concatenate(const std::vector<int>& a, const std::vector<int>& b) {
+        std::vector<int> result = a;
+        result.insert(result.end(), b.begin(), b.end());
+        return result;
+    }
+};
