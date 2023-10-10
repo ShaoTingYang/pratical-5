c015e136997e9d10d41951aa1d7094d1781ad537
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 15:59:10 +0800

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,7 +1,19 @@
-#include "BubbleSort.h"
+#include <vector>
 
-std::vector<int> BubbleSort::sort(std::vector<int> list) {
-    // Implement the Bubble Sort algorithm here
-    // ...
-    return list;
-}
+class BubbleSort {
+public:
+    static std::vector<int> sort(std::vector<int> list) {
+        int n = list.size();
+        bool swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (list[i] > list[i + 1]) {
+                    std::swap(list[i], list[i + 1]);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+        return list;
+    }
+};
