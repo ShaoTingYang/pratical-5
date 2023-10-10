d2f094227dcffd0fd85cb0997330a9dfc7b1ddf4
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-11 00:27:03 +0800

    Update RecursiveBinarySearch.cpp

--- RecursiveBinarySearch.cpp
+++ RecursiveBinarySearch.cpp
@@ -1,7 +1,5 @@
 #include "RecursiveBinarySearch.h"
 
 bool RecursiveBinarySearch::search(std::vector<int> list, int target) {
-    // Implement the recursive binary search algorithm here
-    // ...
     return false;
 }
