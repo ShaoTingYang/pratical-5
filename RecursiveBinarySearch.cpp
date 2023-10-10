b6503b4dcaa05427eec92884032d6e2964662506
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-11 00:42:45 +0800

    Update RecursiveBinarySearch.cpp

--- RecursiveBinarySearch.cpp
+++ RecursiveBinarySearch.cpp
@@ -1,5 +1,20 @@
 #include "RecursiveBinarySearch.h"
 
-bool RecursiveBinarySearch::search(std::vector<int> list, int target) {
+bool binarySearch(std::vector<int>& list, int low, int high, int target) {
+    if (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (list[mid] == target) {
+            return true;
+        }
+        if (list[mid] < target) {
+            return binarySearch(list, mid + 1, high, target);
+        }
+        return binarySearch(list, low, mid - 1, target);
+    }
     return false;
+}
+
+bool RecursiveBinarySearch::search(std::vector<int> list, int target) {
+    int n = list.size();
+    return binarySearch(list, 0, n - 1, target);
 }
